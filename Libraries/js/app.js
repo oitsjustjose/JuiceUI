1193d70d904573f253eabfdecf189b4d89070e8e
oitsjustjose <stovallj1995@gmail.com>
2020-02-27 17:02:52 -0500

    Mobile-friendly Menu UI

--- Libraries/js/app.js
+++ Libraries/js/app.js
@@ -1,7 +1,23 @@
-$(document).ready(function () {
-    $('.dropdown-submenu a.test').on("click", function (e) {
-        $(this).next('ul').toggle();
-        e.stopPropagation();
-        e.preventDefault();
+const toggleSubMenu = (el) => {
+    $(`[for="${el.getAttribute("from")}"`).toggle("slide", "down");
+
+    // TODO: do something with these
+};
+
+const toggleMenu = () => {
+    $(".mobile-menu").toggle("slide", "left");
+};
+
+$(document).ready(() => {
+    document.querySelectorAll(".compact.submenu.item").forEach((el) => {
+        let id = `subMenu_${new Date().getMilliseconds()}`;
+        el.setAttribute("from", id);
+
+        el.querySelectorAll(".compact.sub.item").forEach((child) => {
+            child.setAttribute("for", id);
+            child.style.display = "none";
+            child.textContent = `${child.textContent}`;
+            el.parentElement.appendChild(child);
+        });
     });
 });
