8428856d523ff25a3b997648b6aa06a5892a1ee7
oitsjustjose <stovallj1995@gmail.com>
2020-02-27 18:05:34 -0500

    Animate the dropdown in the mobile menu

--- Libraries/js/app.js
+++ Libraries/js/app.js
@@ -1,7 +1,38 @@
+const rotate = ($elem, angle) => {
+    $({ deg: 0 }).animate({ deg: angle }, {
+        duration: 150,
+        step: function (now) {
+            $elem.css({
+                transform: 'rotate(' + now + 'deg)'
+            });
+        }
+    });
+};
+
+const unrotate = ($elem, angle) => {
+    $({ deg: angle }).animate({ deg: 0 }, {
+        duration: 150,
+        step: function (now) {
+            $elem.css({
+                transform: 'rotate(' + now + 'deg)'
+            });
+        }
+    });
+};
+
 const toggleSubMenu = (el) => {
     $(`[for="${el.getAttribute("from")}"`).toggle("slide", "down");
 
-    // TODO: do something with these
+    let icon = el.querySelector("i.fa");
+    let isOpen = icon.hasAttribute("submenuOpen") ? parseInt(icon.getAttribute("submenuOpen")) : 0;
+
+    if (isOpen == 1) {
+        unrotate($(icon), 90);
+        icon.setAttribute("submenuOpen", 0);
+    } else { // Not open..
+        rotate($(icon), 90);
+        icon.setAttribute("submenuOpen", 1);
+    }
 };
 
 const toggleMenu = () => {
