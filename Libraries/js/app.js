20fcd3d8a7a23f269e9249a3c967746ed1a66183
oitsjustjose <stovallj1995@gmail.com>
2020-02-26 22:07:21 -0500

    --

---
+++ Libraries/js/app.js
@@ -0,0 +1,19 @@
+const isMobile = () => {
+    return window.innerWidth < 767;
+}
+
+$(document).ready(() => {
+    if (isMobile()) {
+        // alert("'Detected' mobile device; fixing things up");
+        let titlebars = document.querySelectorAll(".titlebar");
+
+        titlebars.forEach((titlebar) => {
+
+            // titlebar.childNodes.forEach((child)) => {
+
+            // };
+        });
+    }
+
+});
+
