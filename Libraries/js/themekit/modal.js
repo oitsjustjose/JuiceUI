03e6eccb715afa2d25ccf7389e273d44c93b4bbd
oitsjustjose <stovallj1995@gmail.com>
2020-02-27 22:57:16 -0500

    Improved modals and embedding!

--- Libraries/js/themekit/modal.js
+++ Libraries/js/themekit/modal.js
@@ -2,23 +2,24 @@ $(document).ready(() => {
     // Close when clicking on *not* the modal-contents
     window.onclick = (event) => {
         if (event.target.classList.contains("modal")) {
-            $(event.target).slideUp(150);
+            event.target.style.visibility = 'hidden';
+            event.target.style.opacity = 0;
             document.body.classList.remove("no-scroll");
         }
     };
 
     document.querySelectorAll(".modal .close").forEach((closeBtn) => {
         closeBtn.addEventListener('click', (evt) => {
-            $(closeBtn.parentElement.parentElement).slideUp(150);
-            // closeBtn.parentElement.parentElement.style.display = 'none';
+            closeBtn.parentElement.parentElement.style.visibility = 'hidden';
+            closeBtn.parentElement.parentElement.style.opacity = 0;
             document.body.classList.remove("no-scroll");
         });
     });
 
     document.querySelectorAll("[for-modal]").forEach((modalTrigerEl) => {
         modalTrigerEl.addEventListener('click', (evt) => {
-            $(`#${modalTrigerEl.getAttribute("for-modal")}`).slideDown(150);
-            // document.getElementById().style.display = "block";
+            document.getElementById(modalTrigerEl.getAttribute("for-modal")).style.visibility = "visible";
+            document.getElementById(modalTrigerEl.getAttribute("for-modal")).style.opacity = 1;
             document.body.classList.add("no-scroll");
         });
     });
