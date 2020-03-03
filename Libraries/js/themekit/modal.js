c020aa9b7838dbdc4e45570d630bea99585da286
oitsjustjose <stovallj1995@gmail.com>
2020-03-02 23:22:36 -0500

    Grid fixes and such.

--- Libraries/js/themekit/modal.js
+++ Libraries/js/themekit/modal.js
@@ -2,26 +2,36 @@ $(document).ready(() => {
     // Close when clicking on *not* the modal-contents
     window.onclick = (event) => {
         if (event.target.classList.contains("modal")) {
-            event.target.style.visibility = 'hidden';
-            event.target.style.opacity = 0;
+            $(event.target).hide("fade", {
+                direction: "out",
+            }, 300);
             document.body.classList.remove("no-scroll");
         }
     };
 
     document.querySelectorAll(".modal .close").forEach((closeBtn) => {
         closeBtn.addEventListener('click', (evt) => {
-            closeBtn.parentElement.parentElement.style.visibility = 'hidden';
-            closeBtn.parentElement.parentElement.style.opacity = 0;
+            $(closeBtn.parentElement.parentElement).hide("fade", {
+                direction: "out",
+            }, 300);
             document.body.classList.remove("no-scroll");
         });
     });
 
     document.querySelectorAll("[for-modal]").forEach((modalTrigerEl) => {
         modalTrigerEl.addEventListener('click', (evt) => {
-            document.getElementById(modalTrigerEl.getAttribute("for-modal")).style.visibility = "visible";
-            document.getElementById(modalTrigerEl.getAttribute("for-modal")).style.opacity = 1;
+            // document.getElementById(modalTrigerEl.getAttribute("for-modal")).style.display = "flex";
+            $(`#${modalTrigerEl.getAttribute("for-modal")}`).show("fade", {
+                direction: "in",
+            }, 300);
             document.body.classList.add("no-scroll");
         });
+    });
+});
+
+$(document).ready(() => {
+    document.querySelectorAll("[for-modal]").forEach((modalTrigerEl) => {
+        $(`#${modalTrigerEl.getAttribute("for-modal")}`).hide();
     });
 });
 
